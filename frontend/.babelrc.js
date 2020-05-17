3c7faeabe4a7d53040d0307db29cd9b87b6092bd
Samu <samu@Samus-MacBook-Pro.local>
2020-05-17 16:29:20 +0200

    adding vs code tasks and fix babelrc of frontend

--- frontend/.babelrc.js
+++ frontend/.babelrc.js
@@ -1,4 +1,3 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties', ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: false }]]
 }
